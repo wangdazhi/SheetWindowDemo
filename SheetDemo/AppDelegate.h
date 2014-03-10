2855560c9f0c82317aea2a4365a84b79cad50cc6
Springday <wangdazhitech@springdaynet.local>
2014-03-10 17:12:57 +0800

    Initial Commit

---
+++ SheetDemo/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  SheetDemo
+//
+//  Created by wangdazhitech on 3/10/14.
+//  Copyright (c) 2014 wangdazhitech. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ SheetDemo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SheetDemo
+//
+//  Created by wangdazhitech on 3/10/14.
+//  Copyright (c) 2014 wangdazhitech. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
